04be21cc4e6fa6be0c2050d0d264cf78c7d18141
mystox <mystox@163.com>
2020-05-06 17:34:55 +0800

    [add] 添加资产管理系统页面

---
+++ src/main/java/tech/mystox/asset/AssetManagerApplication.java
@@ -0,0 +1,13 @@
+package tech.mystox.asset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssetManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/mystox/asset/AssetManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.mystox.asset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
